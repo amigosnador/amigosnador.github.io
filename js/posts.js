402bbb2427d71c7e45c29decbe7405e3524df44a
rodrigo-aniceto <rodrigo_anicetoo@yahoo.com.br>
2024-07-25 16:52:06 -0300

    novas imagens para textos

--- js/posts.js
+++ js/posts.js
@@ -4,6 +4,7 @@ const posts = [
     "autor": "Julie Ganschow",
     "data": "00-00-0000",
     "url": "blog/sofrimento/onde_buscar_socorro_quando_estou_sofrendo.html",
+    "imagem": "imgs/blog/sofrendo.jpeg",
     "conteudo": `Frequentemente, no sofrimento, buscamos amigos e familiares para compartilhar nossa dor. Contamos com aqueles que amamos e em quem confiamos para nos ajudar durante o período de sofrimento. Podem ser os pais ou um amigo próximo, um pequeno grupo de estudo; queremos contar nossa história`
     },
     {
@@ -11,6 +12,7 @@ const posts = [
     "autor": "Shannon Kay McCoy",
     "data": "00-00-0000",
     "url": "blog/confianca/confiando_em_deus_nas_tempestades_da_vida.html",
+    "imagem": "imgs/blog/tempestades-da-vida.jpeg",
     "conteudo": "Quando eu era criança crescendo na Carolina do Sul, eu adorava tempestades. Eu observava o vento uivante soprar as folhas dos galhos das árvores e as gotas de chuva baterem na grama sob o céu escuro e cinzento. Se estivéssemos na casa da minha avó, ela nos fazia desligar tudo elétrico."
     },
     {
@@ -18,6 +20,7 @@ const posts = [
     "autor": "Andrew Dealy",
     "data": "00-00-0000",
     "url": "blog/confianca/confiando_no_tempo_e_na_provisao_de_deus.html",
+    "imagem": "imgs/blog/tempestades-da-vida.jpeg",
     "conteudo": "E quando o pegou, ele o colocou na prisão, entregando-o a quatro esquadrões de soldados para guardá-lo, com a intenção de trazê-lo diante do povo depois da Páscoa. Assim, Pedro estava mantido na prisão, mas a igreja orava fervorosamente por ele a Deus. E quando Herodes estava prestes a trazê-lo para"
     },
     {
@@ -25,6 +28,7 @@ const posts = [
     "autor": "Dave Dunham",
     "data": "00-00-0000",
     "url": "blog/confianca/deus_conhece_nossas_duvidas.html",
+    "imagem": "imgs/blog/tempestades-da-vida.jpeg",
     "conteudo": "A insegurança em nossa relação com Deus é uma experiência bastante comum. Enquanto muitos de nós podem ser tranquilizados pelo amor de Deus com facilidade, para algumas pessoas, a ansiedade sobre sua salvação pode ser tão intensa que nenhum nível de tranquilização pode proporcionar conforto. Em tais"
     },
     {
@@ -32,6 +36,7 @@ const posts = [
     "autor": "Paul Tautges",
     "data": "00-00-0000",
     "url": "blog/depressao/busque_o_senhor_nos_tempos_de_angustia.html",
+    "imagem": "imgs/blog/tempo-de-Deus.jpeg",
     "conteudo": ""
     },
     {
@@ -39,6 +44,7 @@ const posts = [
     "autor": "Jeff Christianson",
     "data": "00-00-0000",
     "url": "blog/depressao/jesus_cristo_e_a_nossa_esperanca.html",
+    "imagem": "imgs/blog/tempestades-da-vida.jpeg",
     "conteudo": ""
     },
     {
@@ -46,6 +52,7 @@ const posts = [
     "autor": "Kyle Johnston",
     "data": "00-00-0000",
     "url": "blog/depressao/perseverando_atraves_da_tristeza_em_oracao.html",
+    "imagem": "imgs/blog/oracao.jpeg",
     "conteudo": ""
     },
     {
@@ -53,6 +60,7 @@ const posts = [
     "autor": "Anne Dryburgh",
     "data": "00-00-0000",
     "url": "blog/depressao/solidao.html",
+    "imagem": "imgs/blog/solidao.jpeg",
     "conteudo": ""
     }
 
@@ -66,7 +74,8 @@ document.addEventListener("DOMContentLoaded", function() {
         postElement.classList.add('post');
         postElement.innerHTML = `
         <a href="${post.url}">
-            <img class="ilustracao-post" src="imgs/blog/tempestades-da-vida.jpeg" alt="postagem">
+            
+            <img class="ilustracao-post" src="${post.imagem}" alt="postagem">
         </a>
         <h4><a href="${post.url}">${post.titulo}</a></h4>
             <p>${post.data} <i>${post.autor}</i></p>
@@ -75,3 +84,4 @@ document.addEventListener("DOMContentLoaded", function() {
     });
 });
 
+//<img class="ilustracao-post" src="" alt="postagem"></img>
